acd52b0b99f38371c77ed32aceaa0d2cefc82f90
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-08 14:25:16 -0400

    added organisation

---
+++ app/scripts/organs.js
@@ -0,0 +1,12 @@
+[
+{
+"login": "tiy-atlanta-js",
+"id": 7996991,
+"url": "https://api.github.com/orgs/tiy-atlanta-js",
+"repos_url": "https://api.github.com/orgs/tiy-atlanta-js/repos",
+"events_url": "https://api.github.com/orgs/tiy-atlanta-js/events",
+"members_url": "https://api.github.com/orgs/tiy-atlanta-js/members{/member}",
+"public_members_url": "https://api.github.com/orgs/tiy-atlanta-js/public_members{/member}",
+"avatar_url": "https://avatars.githubusercontent.com/u/7996991?v=2"
+}
+]

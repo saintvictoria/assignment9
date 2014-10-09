bccce408677c90e53501d1510532951a21a1c5c2
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-09 00:07:32 -0400

    first pluck works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,13 @@ console.log('The Iron Yard Rocks');
 _.templateSettings = {
   interpolate: /\{\{(.+?)\}\}/g
 };
+
+var temp1 = $('#repo-temp').first()[0].innerText;
+console.log('temp1=',temp1);
+
+var template = _.template(temp1);
+var repo1html = template({
+  'repo': repos[0]
+  });
+console.log('repo1=',repo1html);
+$('#repo_list').html(repo1html);

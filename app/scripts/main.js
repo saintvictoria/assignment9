c3bbf81952beadbd875ccbaf1ba443f31fd5f98c
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-10 12:14:05 -0400

    pulled in repo data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,33 +3,35 @@ _.templateSettings = {
   interpolate: /\{\{(.+?)\}\}/g
 };
 var url = 'https://api.github.com/users/saintvictoria';
+var urlrepo = 'https://api.github.com/users/saintvictoria/repos';
+var urlorg ='https://api.github.com/users/saintvictoria/orgs';
 
-var temp1 = $('#repo-temp').first()[0].innerText;
-console.log('temp1=',temp1);
+var repoTemplate =$('#repo-temp').text(),
+    renders = _.template(repoTemplate);
 
-var template = _.template(temp1);
+    $.getJSON(urlrepo).done(function (repo_data){
 
-_.each(repos, function(repo) {
+      _.each(repo_data, function(x){
+        $('#repo_list').append(renders(x));
 
-repo.days_ago=3;
-var repo1html = template({
-  'repo': repo
+  })
   });
-console.log('repo1=',repo1html);
-var $li = $('<li></li>');
-$li.html(repo1html);
-$('#repo_list').append($li);
-
-});
-
 
 
 var template = $('#user_stuff').html(),
     render = _.template(template);
 
-$.getJSON(url).done( function (user_data){
+    $.getJSON(url).done( function (user_data){
 
 
-    $('#sidebar').append(render(user_data));
+      $('#sidebar').append(render(user_data));
 
   });
+
+var orgTemplate = $('#org_stuff').html(),
+    rendered =_.template(orgTemplate);
+
+      $.getJSON(urlorg).done( function (org_data){
+
+        $('#undersidebar').append(rendered(org_data));
+      });
